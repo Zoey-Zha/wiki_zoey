28e81121c944feda7ea871603855c13e3ea0f168
Zoey-Zha <xinzha92@gmail.com>
2022-01-05 00:49:11 +0800

    2-6 开发Hello World接口

---
+++ src/main/java/com/zoey/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.zoey.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.POST)
+    public String hello() {
+        return "hello!";
+    }
+}
