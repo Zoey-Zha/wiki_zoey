24afaad5e81710646925ae82d0672968a65a4738
Zoey-Zha <xinzha92@gmail.com>
2022-01-04 21:11:59 +0800

    2-2 init

---
+++ src/test/java/com/zoey/WikiZoeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiZoeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
