20eb2362044d7156a87bfa4cfa03aeee201882ba
Zoey-Zha <xinzha92@gmail.com>
2022-01-08 17:30:28 +0800

    3-5 Mybatis自动生成，wiki database下添加demo表

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,17 @@ create table test (
     primary key (id)
 ) engine = innodb default
 charset = utf8mb4 comment='test';
+
+drop table if exists demo;
+
+
+------
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment 'name',
+                        primary key (id)
+) engine = innodb default
+charset = utf8mb4 comment='test';
+
+insert into demo values(1001,'Alix');
+insert into demo values(1002,'Zoey');
