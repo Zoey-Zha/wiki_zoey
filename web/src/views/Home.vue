f2723d912fffe847980308b434d9fee9acdfd43a
Zoey-Zha <xinzha92@gmail.com>
2022-01-12 00:15:17 +0800

    5-2 1. 更改后端端口8090（避免与前端端口冲突） 2. 前端调用后端ebook接口测试console打印数据 3. 增加前后端跨域处理类

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,8 +3,6 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
       >
         <a-sub-menu key="sub1">
@@ -52,5 +50,22 @@
     </a-layout-content>
   </a-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent }  from 'vue';
+import axios from "axios";
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    console.log("setup");
+    axios.get("http://localhost:8090/ebook/ebookList?name=Spring").then(
+        (response) => {
+          console.log(response);
+        }
+    )
+  }
+});
+</script>
 
 
