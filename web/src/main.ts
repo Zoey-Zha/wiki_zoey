a2455041e3ec60c6aac07c6198cea5bd4302d6ab
Zoey-Zha <xinzha92@gmail.com>
2022-01-13 00:51:26 +0800

    5-6 配置axios拦截器打印请求参数和返回参数

--- web/src/main.ts
+++ web/src/main.ts
@@ -8,6 +8,23 @@ import axios from 'axios';
 
 axios.defaults.baseURL=process.env.VUE_APP_SERVER_URL
 
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
+
 
 createApp(App).use(store).use(router).use(Antd).mount('#app');
 console.log("env: " + process.env.NODE_ENV);
