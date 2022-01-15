9de8f58b32a5dd2ad5c5ba96250cc15a3884c993
Zoey-Zha <xinzha92@gmail.com>
2022-01-15 20:50:11 +0800

    5-9 AOP删除一些无关code后，测试成功 6-2 增加电子书管理页面，增加路由、菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import AdminEbook from '../views/admin/AdminEbook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,12 +10,18 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
+  },
+  {
     path: '/about',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   }
 ]
 
