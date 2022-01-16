9697d078181d6126267f3c54e830657506fb3d42
Zoey-Zha <xinzha92@gmail.com>
2022-01-16 12:09:10 +0800

    6-4 PageHelper实现后端分页

--- src/main/java/com/zoey/service/EbookService.java
+++ src/main/java/com/zoey/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.zoey.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zoey.domain.Ebook;
 import com.zoey.domain.EbookExample;
 import com.zoey.mapper.EbookMapper;
 import com.zoey.reps.EbookResp;
 import com.zoey.req.EbookReq;
 import com.zoey.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -14,6 +18,9 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    // private static final Logger logger = LoggerFactory.getLogger(EbookService.class);
+    private static final Logger logger = LoggerFactory.getLogger(EbookService.class);
 
     @Autowired
     private EbookMapper ebookMapper;
@@ -24,14 +31,24 @@ public class EbookService {
 //    }
 
     public List<EbookResp> getList(EbookReq ebookReq) {
+
+
+
         EbookExample example = new EbookExample();
         EbookExample.Criteria criteria = example.createCriteria();
         // 增加动态SQL
         if (!ObjectUtils.isEmpty(ebookReq.getName())) {
             criteria.andNameLike("%" + ebookReq.getName() + "%");
         }
+
+        // 后端分页，写在要分页的查询前面，因为它只对一个sql查询生效
+        PageHelper.startPage(2,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(example);
 
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        logger.info("Total rows: {}", pageInfo.getTotal());
+        logger.info("Total pages: {}", pageInfo.getPages());
+        logger.info("Page num: {}", pageInfo.getPageNum());
 
         // List<EbookResp> ebookResps = new ArrayList<>();
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
