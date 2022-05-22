ed6ea9459d88b4c1c7079e8c0bf2bd6f95ddb308
Zoey-Zha <xinzha92@gmail.com>
2022-05-23 02:49:36 +0800

    10-6 1. 增加定时任务日志打印和耗时日志打印

--- src/main/java/com/zoey/job/DocJob.java
+++ src/main/java/com/zoey/job/DocJob.java
@@ -1,6 +1,8 @@
 package com.zoey.job;
 
 import com.zoey.service.DocService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +11,7 @@ import javax.annotation.Resource;
 @Component
 public class DocJob {
 
-    // private static final Logger LOG = (Logger) LoggerFactory.getLogger(DocJob.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
     @Resource
     private DocService docService;
@@ -17,9 +19,14 @@ public class DocJob {
     /**
      * update at noon every day
      */
-    @Scheduled(cron = "0 0 12 * * ??")
+     @Scheduled(cron = "0 0 12 * * ??")
+//    @Scheduled(cron = "0/5 * * * * ? ")
     public void cron() {
+        LOG.info("update ebook every noon");
+        long start = System.currentTimeMillis();
         docService.updateEbookInfo();
+        // 关注下任务耗时
+        LOG.info("updating finished, duration: {} 毫秒", System.currentTimeMillis() - start);
     }
 
 }
