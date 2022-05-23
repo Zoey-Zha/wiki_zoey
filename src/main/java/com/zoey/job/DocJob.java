2395742fb21dde028c2315ddbf82b9ccba7ca1aa
Zoey-Zha <xinzha92@gmail.com>
2022-05-23 14:02:54 +0800

    10-7 1. 增加日志流水号，方便运维 2. 增加新的Aspect拦截器，对定时任务增加日志流水号

--- src/main/java/com/zoey/job/DocJob.java
+++ src/main/java/com/zoey/job/DocJob.java
@@ -1,6 +1,7 @@
 package com.zoey.job;
 
 import com.zoey.service.DocService;
+import com.zoey.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,17 +17,22 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * update at noon every day
      */
-     @Scheduled(cron = "0 0 12 * * ??")
-//    @Scheduled(cron = "0/5 * * * * ? ")
+//     @Scheduled(cron = "0 0 12 * * ?")
+    @Scheduled(cron = "0/5 * * * * ? ")
     public void cron() {
-        LOG.info("update ebook every noon");
-        long start = System.currentTimeMillis();
-        docService.updateEbookInfo();
-        // 关注下任务耗时
-        LOG.info("updating finished, duration: {} 毫秒", System.currentTimeMillis() - start);
+         // 增加日志流水号
+//         MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+         LOG.info("update ebook every noon");
+         long start = System.currentTimeMillis();
+         docService.updateEbookInfo();
+         // 关注下任务耗时
+         LOG.info("updating finished, duration: {} 毫秒", System.currentTimeMillis() - start);
     }
 
 }
